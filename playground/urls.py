39b248594b7977ecde5cafc76505fbf1f27ac85d
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-09-12 20:18:22 +0100

    everything is working fine

--- playground/urls.py
+++ playground/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.say_hello),  # Root URL will point to 'say_hello' view
     path('hello/', views.say_hello),
     path('calculate/', views.calculate),
     path('templates/', views.usingTemplates)
 ]
+
--- store/models.py
+++ store/models.py
@@ -3,40 +3,36 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6,decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
-    last_update = models.DateField(auto_now = True)
+    last_update = models.DateField(auto_now=True)
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
     MEMBERSHIP_GOLD = 'G'
-    MEMBERSHIP_CHOICES =[
-        ('MEMBRSHIP_BRONZE', 'Bronze'),
-        ('MEMBERSHIP_SILVER', 'Silver'),
-        ('MEMBERSHIP_GOLD', 'Gold'),
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
     ]
-    firstName = models.CharField(255)
-    lastName = models.CharField(255)
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birthDate = models.DateTimeField(null=True)
-    membership = models.CharField(max_length=1,choices=MEMBERSHIP_CHOICES,default=MEMBERSHIP_BRONZE)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
     PAYMENT_STATUS_COMPLETE = 'C'
     PAYMENT_STATUS_FAILED = 'F'
     PAYMENT_STATUS_CHOICES = [
-        ('PAYMENT_STATUS_PENDING', 'Pending'),
-        ('PAYMENT_STATUS_COMPLETE', 'Complete'),
-        ('PAYMENT_STATUS_FAILED', 'Failed')
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
     ]
     placed_at = models.DateField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
-
-
-
