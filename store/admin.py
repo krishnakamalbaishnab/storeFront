b508e5baf92a71e06db84f3bf68ffe8fc5d6d2b8
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-09-28 23:34:23 +0100

    connecting links to other pages within admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.db.models.aggregates import Count
 # from django.db.models.query import QuerySet
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 from . import models
 
 # Register your models here.
@@ -43,7 +45,13 @@ class CollectionAdmin(admin.ModelAdmin):
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
-        return collection.products_count
+        url = (
+            reverse('admin:store_product_changelist')
+            + '?'
+            + urlencode({
+                'collection__id': str(collection.id)
+            }))
+        return format_html('<a href="{}">{} Products</a>', url, collection.products_count)
     
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
