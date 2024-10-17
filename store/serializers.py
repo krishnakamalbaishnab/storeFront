607eefebe969e788e8404b3ac950e45c29d97479
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-10-17 09:42:13 +0100

    Validating Data and Deserializing objects

--- store/serializers.py
+++ store/serializers.py
@@ -2,15 +2,21 @@ from decimal import Decimal
 from store.models import Product, Collection
 from rest_framework import serializers
 
-class ProductSerializers(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6,decimal_places=2)
-    priceWithTax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.PrimaryKeyRelatedField(
-        queryset = Collection.objects.all()
-    )
+class ProductSerializers(serializers.ModelSerializer):
 
+    #another method of writing the model serializer
+    class Meta:
+        model = Product
+        fields = ['id','title','price','priceWithTax','collection']
+
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(max_digits=6,decimal_places=2)
+    # priceWithTax = serializers.SerializerMethodField(method_name='calculate_tax')
+    # collection = serializers.PrimaryKeyRelatedField(
+    #     queryset = Collection.objects.all()
+    # )
+    priceWithTax = serializers.SerializerMethodField(method_name='calculate_tax')
     def calculate_tax(self,product: Product):
         return product.price * Decimal(1.1)
 
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -7,15 +7,25 @@ from .models import Product
 from .serializers import ProductSerializers
 
 # Create your views here.
-@api_view()
+@api_view(['GET','POST'])
+
 def product_list(request):
-    queryset= Product.objects.all()
-    serializer = ProductSerializers(queryset, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        queryset= Product.objects.all()
+        serializer = ProductSerializers(queryset, many=True, context={'request':request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializers(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+             serializer.validated_data
+             return Response('OKAY')
+        # else:
+        #      return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)  //don't need it if we are using raiseException
+
 
 @api_view()
 def product_details(request, id):
-        product = get_list_or_404(Product,pk=id)
+        product = get_object_or_404(Product,pk=id)
         serializers = ProductSerializers(product)
         return Response(serializers.data)
     
