4515410774ce2602e87cedfd5f330ee08c825bb6
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-10-17 13:20:27 +0100

    Collection List API Created

--- store/models.py
+++ store/models.py
@@ -28,8 +28,8 @@ class Product(models.Model):
         validators= [MinValueValidator(1)])
     inventory = models.IntegerField(validators=[MinValueValidator(0)])
     last_update = models.DateField(auto_now=True)
-    # collection = models.ForeignKey(Collection,on_delete=models.PROTECT)
-    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, null=True, blank=True)
+    collection = models.ForeignKey(Collection,on_delete=models.PROTECT, related_name='products')
+    # collection = models.ForeignKey(Collection, on_delete=models.CASCADE, null=True, blank=True)
     promotions = models.ManyToManyField(Promotion,blank=True)
 
     def __str__(self) -> str:
@@ -75,7 +75,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='orderitem')
     quantity = models.PositiveIntegerField()
     unitPrice = models.DecimalField(max_digits=10, decimal_places=2)
 
--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('products/', views.product_list),
     path('products/<int:id>/',views.product_details),
+    path('collection',views.collection_list),
+    path('collection/<int:id>/',views.collection_list)
 ]
 
